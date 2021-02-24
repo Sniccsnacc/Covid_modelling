c06820957c3546785edb1b35622eb8ca048d1283
Sniccsnacc <Lauritsnicolaisen@outlook.com>
2021-02-24 09:33:47 +0100

    Update Latatouille.py

--- Latatouille.py
+++ Latatouille.py
@@ -2,8 +2,11 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
+from Alex_lege_med_data import data_age, data_time_pos
 
+D_O_T = pd.read_csv('Data/Deaths_over_time.csv',sep = ';')
 Cases_sex = pd.read_csv('Data/Cases_by_sex.csv', sep = ';')
 
 
