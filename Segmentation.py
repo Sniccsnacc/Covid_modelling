c31c7ba51747d66e7ceb4db347c1429424f068c4
AlexHeide98 <74244305+AlexHeide98@users.noreply.github.com>
2021-06-06 16:11:12 +0200

    Segmentering, men med overflow

--- Segmentation.py
+++ Segmentation.py
@@ -4,15 +4,15 @@ from scipy.integrate import odeint
 from Models import*
 
 num_regions = 3
-betas = [1, 2, 3]
-gammas = [1, 2, 3]
-rhos = [1, 2, 3]
+betas = [1/2, 1/2, 1/2]
+gammas = [1/7, 1/7, 1/7]
+rhos = [1, 1, 1]
 
 B = np.zeros((num_regions, num_regions))
 np.fill_diagonal(B, betas)
 
-R = np.zeros((num_regions, num_regions))
-np.fill_diagonal(R, rhos)
+P = np.zeros((num_regions, num_regions))
+np.fill_diagonal(P, rhos)
 
 C = np.zeros((num_regions, num_regions))
 np.fill_diagonal(C, gammas)
@@ -25,17 +25,16 @@ def SIR_SEG(z, t):
     I = np.zeros((r, l+1))
     R = np.zeros((r, l+1))
     S[:, 0] = z[:, 0]
-    I[:, 0] = z[:, 0]
-    R[:, 0] = z[:, 0]
-
+    I[:, 0] = z[:, 1]
+    R[:, 0] = z[:, 2]
     for i in range(1, t.size):
-        dt = t[i] - t[i-1]
-        dSdt = -B.dot(S[:, i-1] * I[:, i-1]) + R.dot(S[:, i-1])
-        dIdt = B.dot(S[:, i-1] * I[:, i-1]) - C.dot(I[:, i-1]) + R.dot(I[:, i-1])
+        dSdt = -B.dot(S[:, i-1] * I[:, i-1]) + P.dot(S[:, i-1])
+        dIdt = B.dot(S[:, i-1] * I[:, i-1]) - C.dot(I[:, i-1]) + P.dot(I[:, i-1])
         dRdt = C.dot(I[:, i-1])
-        S[:, i] = S[:, i-1] + dSdt * dt
-        I[:, i] = I[:, i-1] + dIdt * dt
-        R[:, i] = R[:, i-1] + dRdt * dt
+        print(dSdt, dIdt, dRdt)
+        S[:, i] = S[:, i-1] + dSdt
+        I[:, i] = I[:, i-1] + dIdt
+        R[:, i] = R[:, i-1] + dRdt
 
     return S, I, R
 
@@ -44,7 +43,7 @@ I0 = np.ones(num_regions)
 R0 = np.zeros(num_regions)
 S0 = np.ones(num_regions) * (N - 1)
 Z0 = np.transpose(np.array([S0, I0, R0]))
-
+print(Z0)
 S, I, R = SIR_SEG(Z0, t)
 
 
