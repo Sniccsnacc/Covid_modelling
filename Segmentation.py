d4d00f22793bfb2449adb7f5acfebe1ba146349d
AlexHeide98 <74244305+AlexHeide98@users.noreply.github.com>
2021-06-08 09:14:19 +0200

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -1,6 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
-from scipy.integrate import odeint
 from Models import*
 from Kom_dat import *
 
@@ -8,14 +6,14 @@ from Kom_dat import *
 num_regions = 3
 population = np.array([N, N, N])
 gammas = np.array([1/7, 1/7, 1/7])
-travel_out = np.array([0.5, 0, 0])
+travel_out = np.array([0.5, 0.2, 0.4])
 travel_in = np.array([[0, 0.2, 0.8],
-                      [0.2, 0, 0.8],
-                      [0.2, 0.8, 0]])
+                      [0.3, 0, 0.7],
+                      [0.5, 0.5, 0]])
 
 #num_regions = len(travel_out)
 #population = np.array(population)
-#gammas = np.ones(num_regions)
+#gammas = np.ones(num_regions) * 1/7
 #travel_out = np.array(travel_out)
 #travel_in = np.array(travel_in)
 
@@ -32,9 +30,9 @@ def SIR_SEG(z, t):
     R[:, 0] = z[:, 2]
     P[:, 0 ] = S[:, 0] + I[: , 0] + R[:, 0]
     for i in range(1, t.size):
-        dSdt = -beta * S[:, i - 1] * I[:, i - 1] / population - travel_out * S[:, i - 1] + travel_out * (travel_in.dot(S[:, i - 1]))
-        dIdt = beta * S[:, i - 1] * I[:, i - 1] / population - gammas * I[:, i - 1] - travel_out * I[:, i - 1] + travel_out * (travel_in.dot(I[:, i - 1]))
-        dRdt = gammas * I[:, i - 1] - travel_out * R[:, i - 1] + travel_out * (travel_in.dot(R[:, i - 1]))
+        dSdt = -beta * S[:, i - 1] * I[:, i - 1] / population - travel_out * S[:, i - 1] + (travel_out * travel_in.T).dot(S[:, i - 1])
+        dIdt = beta * S[:, i - 1] * I[:, i - 1] / population - gammas * I[:, i - 1] - travel_out * I[:, i - 1] + (travel_out * travel_in.T).dot(I[:, i - 1])
+        dRdt = gammas * I[:, i - 1] - travel_out * R[:, i - 1] + (travel_out * travel_in.T).dot(R[:, i - 1])
 
         S[:, i] = S[:, i-1] + dSdt
         I[:, i] = I[:, i-1] + dIdt
@@ -43,9 +41,10 @@ def SIR_SEG(z, t):
     return S, I, R, P
 
 
-I0 = np.ones(num_regions)
+#I0 = np.ones(num_regions)
+I0 = np.array([10, 0, 5])
 R0 = np.zeros(num_regions)
-S0 = np.ones(num_regions) * (N - 1)
+S0 = population - I0
 Z0 = np.transpose(np.array([S0, I0, R0]))
 ts = t.size
 
@@ -57,7 +56,7 @@ plt.plot(t, I[0, 0:ts], color = '#228B22', label='I')
 plt.plot(t, R[0, 0:ts], color = '#B22222', label='R')
 plt.plot(t, P[0, 0:ts], label='P')
 plt.ylabel('# Mennesker')
-plt.legend(loc='best')
+#plt.legend(loc='best')
 plt.grid()
 
 fig1.add_subplot(3, 1, 2)
@@ -66,7 +65,7 @@ plt.plot(t, I[1, 0:ts], color = '#228B22', label='I')
 plt.plot(t, R[1, 0:ts], color = '#B22222', label='R')
 plt.plot(t, P[1, 0:ts], label='P')
 plt.ylabel('# Mennesker')
-plt.legend(loc='best')
+#plt.legend(loc='best')
 plt.grid()
 
 fig1.add_subplot(3, 1, 3)
@@ -74,9 +73,9 @@ plt.plot(t, S[2, 0:ts], color = '#00BFFF', label='S')
 plt.plot(t, I[2, 0:ts], color = '#228B22', label='I')
 plt.plot(t, R[2, 0:ts], color = '#B22222', label='R')
 plt.plot(t, P[2, 0:ts], label='P')
+plt.xlabel('t [dage]')
 plt.ylabel('# Mennesker')
-plt.legend(loc='best')
+#plt.legend(loc='best')
 plt.grid()
 plt.tight_layout()
-
 plt.show()
